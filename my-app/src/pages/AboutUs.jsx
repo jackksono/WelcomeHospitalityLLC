588cc2b392878dfba330e84be3c2bb191865b0ec
Jack Sonoda <jackksono@gmail.com>
2023-05-30 23:15:09 -0700

    added home and navbar

--- my-app/src/pages/AboutUs.jsx
+++ my-app/src/pages/AboutUs.jsx
@@ -4,21 +4,22 @@ import React, {useState}  from "react"
 const AboutUs = () => {
 
     return (
-        <div>
-        <div>
-            <img src={logo}></img>
-            <p>Michelle Tran</p>
-            <p1>text</p1>
+        <div className="pt-20">
+        <h1 className="text-2xl">About Us!</h1>
+        <div className="max-w-sm flex flex-col rounded-lg overflow-hidden bg-white shadow">
+            {/* <img src={logo}></img> */}
+            <p className="">Michelle Tran</p>
+            <p1 className="">text</p1>
         </div>
-        <div>
-            <img src={logo}></img>
-            <p>Donna</p>
-            <p1>text</p1>
+        <div className="max-w-sm flex flex-col rounded-lg overflow-hidden bg-white shadow">
+            {/* <img src={logo}></img> */}
+            <p className="">Donna</p>
+            <p1 className="">text</p1>
         </div>
-        <div>
-            <img src={logo}></img>
-            <p>Jack Sonoda</p>
-            <p1>text</p1>
+        <div className="max-w-sm flex flex-col rounded-lg overflow-hidden bg-white shadow">
+            {/* <img className="" src={logo}></img> */}
+            <p className="">Jack Sonoda</p>
+            <p1 className="">text</p1>
         </div>
     </div>
     )
--- my-app/src/pages/NavBar.jsx
+++ my-app/src/pages/NavBar.jsx
@@ -13,9 +13,14 @@ const NavBar = () => {
 
     return (
         <>
-        <nav  className="fixed w-screen bg-secondary-500 h-[80px] z-10 drop-shadow-xl">
+        <nav  className="fixed w-screen bg-orange-300 h-[80px] z-10 drop-shadow-xl">
         <div className="flex justify-between w-full h-full px-20">
           <div className="flex items-center gap-10">
+            <Link to="/">
+              <button className="bg-primary-500 text-secondary-500 hover:text-secondary-700">
+                Home
+              </button>
+            </Link>
           </div>
           <div className="flex items-center gap-10">
             <Link to="/about-us">
