f10e68726a5ca965f1fe83b05d7227365dd35274
Jack Sonoda <jackksono@gmail.com>
2023-08-04 22:48:36 -0700

    Fixed home page; last part adding background for each hover

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -7,18 +7,18 @@ const Home = () => {
     const navigate = useNavigate()
 
     return (
-        <div className="fixed flex items-center justify-center w-screen h-screen bg-stone-900 drop-shadow-xl ">
+        <div className="fixed flex items-center justify-center w-screen h-screen bg-stone-900 drop-shadow-xl">
           <div className="text-center">
-            <h1 className="text-4xl text-white lg:pb-72">
-              Your Experience,<span className="italic"> Refined.</span>
+            <h1 className="text-3xl italic text-white lg:pb-72 font-Nanum">
+              Experienced Professionals and<span className="italic"> Warm Hospitality.</span>
             </h1>
           </div>
               
               
             <div className="absolute bottom-0 left-0 flex items-center justify-center w-full h-full">
               <div className="flex flex-row space-x-64">
-                <h1 className="text-6xl font-bold text-white duration-700 cursor-pointer hover:underline font-Nanum">CONCEPTS</h1>
-                <h2 className="text-6xl font-bold text-white duration-700 cursor-pointer hover:underline">MARKETING</h2>
+                <h1 className="text-7xl font-bold text-white duration-700 cursor-pointer hover:underline font-Nanum underline-offset-[20px]">CONCEPTS</h1>
+                <h2 className="text-7xl font-bold text-white duration-700 cursor-pointer hover:underline font-Nanum underline-offset-[20px]" >MARKETING</h2>
               </div>
             </div>
         </div>
--- my-app/src/pages/NavBar.jsx
+++ my-app/src/pages/NavBar.jsx
@@ -8,50 +8,53 @@ import Logo from '../Images/Logos/Logo.png'
 
 const NavBar = () => {
 
-    const [ selectedPage, setSelectedPage ] = useState()
-    const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const navigate = useNavigate();
 
-    const onToggleMenu = () => {
-      const navLinks = document.querySelector('.nav-links')
+  const onToggleMenu = () => {
+    setMenuOpen((prevMenuOpen) => !prevMenuOpen);
+    const navLinks = document.querySelector('.nav-links')
     // console.log(navLinks)
     let tag = document.getElementById("menu")
     // console.log(tag.name)
     tag.name = tag.name === "close" ? "menu" : "close"
     // console.log(navLinks.classList)
     navLinks.classList.toggle('hidden')
-    }
+  }
 
     return (
         <>
-        <nav  className="fixed w-screen top-0 left-0 bg-transparent h-[150px] z-10 drop-shadow-xl">
-        <div className="flex items-start justify-between w-full h-full">
-          <div className="flex items-center">
-            <Link to="/">
-              <img src={Logo} alt='Logo' className="lg:w-100 lg:h-60 lg:mb-20">
-              </img>
-            </Link>
-          </div>
-          
-          <div className="absolute top-0 mt-4 mr-8 text-4xl text-white duration-1000 lg:right-20 lg:pt-10">
-            <button >
-                <ion-icon name="menu" id="menu" onClick={onToggleMenu} className='focus:duration-1000'></ion-icon>
-              </button>
-          </div>
+          <nav  className="fixed w-screen top-0 left-0 bg-transparent h-[150px] z-10 drop-shadow-xl">
+            <div className="flex items-start justify-between w-full h-full">
+              <div className="flex items-center mr-40">
+                <Link to="/">
+                  <img 
+                    src={Logo} 
+                    alt='Logo' 
+                    className="transform scale-150 lg:w-80 lg:h-40 lg:mb-20">
+                  </img>
+                </Link>
+              </div>
+              
+              <div className="absolute top-0 mt-4 mr-8 text-4xl text-white lg:right-20 lg:pt-10">
+                <button className=''>
+                    <ion-icon name="menu" id="menu" onClick={onToggleMenu} ></ion-icon>
+                  </button>
+              </div>
 
-          <div className="flex flex-row-reverse items-center hidden gap-20 text-lg nav-links lg:pt-16 lg:pr-48">
-          
-              <Link to="/about-us">
-                <button className="text-white">
-                  Executive Leadership
-                </button>
-              </Link>
-              <Link to="/contact-us">
-                <button className="text-white">Contact Us</button>
-              </Link>
-           
-          </div>
-        </div>
-        </nav>
+              <div className={`flex flex-row-reverse items-center gap-10 text-lg nav-links lg:pt-16 lg:pr-48 transition duration-500 ${menuOpen ? 'visible opacity-100' : 'invisible opacity-0'}`}>
+                  <Link to="/about-us">
+                    <button className="text-white font-Nanum">
+                      Executive Leadership
+                    </button>
+                  </Link>
+                  <Link to="/contact-us">
+                    <button className="text-white font-Nanum">Contact Us</button>
+                  </Link>
+              
+              </div>
+            </div>
+          </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact-us" element={<ContactUs/>} />
