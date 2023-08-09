949888918e2a291a7f9d8c3076bc51451b1dd8f6
Jack Sonoda <jackksono@gmail.com>
2023-08-08 22:16:54 -0700

    Finished resizing for AboutUs page

--- my-app/src/pages/AboutUs.jsx
+++ my-app/src/pages/AboutUs.jsx
@@ -6,38 +6,40 @@ const AboutUs = () => {
 
     return (
         <>
-            <div className="flex items-center justify-center overflow-auto text-3xl text-center pt-28 lg:h-1/4 sm:pt-48 lg:pt-80 bg-stone-900 font-Italiana ">
+        <div className="w-screen overflow-auto bg-stone-900">
+            <div className="flex items-center justify-center overflow-auto text-3xl text-center lg:-z-0 pt-28 lg:h-3/4 sm:pt-56 lg:mt-40 bg-stone-900 font-Italiana">
                 <div className="flex flex-col">
-                    <h1 className="text-white lg:text-8xl sm:text-4xl">WHO ARE WE</h1>
-                    <p className="pt-2 sm:pt-2 text-white lg:text-lg md:text-[20px] sm:text-[15px] text-sm sm:m-5 leading-5 sm:leading-[2rem]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis corporis doloremque corrupti dolorem temporibus 
+                    <h1 className="text-white underline lg:text-8xl sm:text-4xl underline-offset-8 lg:mt-0">WHO ARE WE</h1>
+                    <p className="pt-4 mx-5 text-white lg:text-lg md:text-[20px] sm:text-[15px] text-xs sm:mx-5 leading-5 lg:pt-7 lg:m-10 sm:leading-[2rem]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis corporis doloremque corrupti dolorem temporibus 
                         aliquid hic repellat atque eius dolores suscipit, quam vel porro sequi aperiam dolor omnis optio facilis, nemo laudantium
                         voluptatibus, explicabo commodi? Error iusto quasi illo quia?
                     </p>
                 </div>
             </div>
+;
+            <div className="flex justify-center lg:min-h-screen w-screen text-3xl lg:z-10 text-center mt-[-50px] sm:mt-[-100px] sm:pt-0 sm:h-3/4 lg:h-auto bg-stone-900 font-Italiana">
+                <div className="flex flex-col items-center justify-center pt-10 text-center text-white sm:pt-10 sm:text-4xl lg:text-8xl ">
+                    <h1 className="underline lg:z-10 underline-offset-8">MEET THE TEAM</h1>
 
-            <div className="flex items-center justify-center w-screen overflow-auto text-3xl text-center sm:pt-10 sm:h-1/2 lg:h-1/2 bg-stone-900 font-Italiana">
-                <div className="flex flex-col items-center justify-center pt-10 text-center text-white sm:text-4xl lg:text-8xl lg:mt-4">
-                    <h1 className="">MEET THE TEAM</h1>
+                    <img src={TeamPicture} alt="TeamPicture" className="w-3/4 mt-5 rounded-lg shadow-sm sm:w-1/4 sm:pt-10 sm:mt-0 lg:mt-16 lg:w-2/5 lg:pt-0"></img>
 
-                    <img src={TeamPicture} alt="TeamPicture" className="w-3/4 mt-5 rounded-lg shadow-sm sm:w-1/4 sm:pt-10 sm:mt-16 lg:mt-4"></img>
-                    <div className="flex flex-col pt-10 text-center sm:absolute lg:left-24 lg:pb-0 lg:w-1/4 sm:w-1/3 md:pt-0 sm:left-5 sm:pt-10">
-                        <h1 className="lg:text-4xl sm:text-2xl">Michelle Tran</h1>
-                        <label className="italic text-gray-300 lg:text-lg sm:text-xs text-[10px]">Managing Partner</label>
-                        <p className="text-[10px] lg:text-lg lg:w-auto sm:text-sm sm:pt-3 lg:pt-10 md:text-base m-2 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia molestiae rerum 
+                    <div className="flex flex-col pt-5 text-center sm:absolute lg:pb-0 lg:w-1/4 lg:pr-16 lg:mt-40 lg:left-10 sm:w-1/3 md:pt-20 sm:left-5 sm:pt-28">
+                        <h1 className="lg:text-4xl sm:text-3xl mb-[-13px] sm:mb-0">Michelle Tran</h1>
+                        <label className="italic text-gray-300 lg:text-lg sm:text-xs text-[10px] mb-[-5px]">Managing Partner</label>
+                        <p className="text-[10px] lg:text-lg lg:w-auto sm:text-sm sm:pt-3 lg:pt-10 md:text-base mx-3 leading-5 mb-[-5px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia molestiae rerum 
                         quam pariatur, impedit possimus dignissimos rem quidem autem libero incidunt dolor! Soluta, ipsam saepe pariatur dolorum 
                         quos culpa assumenda ab rerum incidunt? Accusamus consequuntur dolor nulla sunt sequi? Voluptatem?</p>
                     </div>
-                    <div className="flex flex-col text-center sm:absolute lg:right-24 lg:pb-0 lg:w-1/4 sm:w-1/3 sm:right-5 sm:pt-10 md:pt-0">
-                        <h1 className="lg:text-4xl sm:text-3xl ">Dorris C</h1>
-                        <label className="italic text-gray-300 lg:text-lg sm:text-xs text-[10px]">Managing Partner</label>
-                        <p className="w-auto text-lg sm:text-sm sm:pt-3 md:text-base lg:pt-10 text-[10px] m-2 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia molestiae rerum 
+                    <div className="flex flex-col pt-5 text-center sm:absolute lg:right-10 lg:pl-16 lg:pb-0 lg:w-1/4 sm:w-1/3 lg:mt-40 sm:right-5 sm:pt-28 md:pt-20">
+                        <h1 className="lg:text-4xl sm:text-3xl mb-[-13px] sm:mb-0">Dorris C</h1>
+                        <label className="italic text-gray-300 lg:text-lg sm:text-xs text-[10px] mb-[-5px]">Managing Partner</label>
+                        <p className="text-[10px] lg:text-lg lg:w-auto sm:text-sm sm:pt-3 lg:pt-10 md:text-base mx-3 leading-5 mb-[-5px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia molestiae rerum 
                         quam pariatur, impedit possimus dignissimos rem quidem autem libero incidunt dolor! Soluta, ipsam saepe pariatur dolorum 
                         quos culpa assumenda ab rerum incidunt? Accusamus consequuntur dolor nulla sunt sequi? Voluptatem?</p>
                     </div>
-                
                 </div>
             </div>
+        </div>
         </>
     )
 }
--- my-app/src/pages/NavBar.jsx
+++ my-app/src/pages/NavBar.jsx
@@ -27,23 +27,23 @@ const NavBar = () => {
         <>
           <nav  className="fixed w-screen top-0 left-0 bg-transparent h-[150px] z-10 drop-shadow-xl">
             <div className="flex items-start justify-between lg:h-full lg:w-full">
-              <div className="flex items-center mt-3 ml-2 mr-10 lg:mr-40">
+              <div className="flex items-center mt-3 ml-2 sm:mr-20 sm:mt-0 lg:mr-40">
                 <Link to="/">
                   <img 
                     src={Logo} 
                     alt='Logo' 
-                    className="transform scale-[2] lg:scale-150 lg:w-80 lg:h-40 lg:mb-20">
+                    className="transform scale-[2] sm:scale-[1.5] lg:scale-[2.5] lg:ml-5 object-cover sm:object-fill lg:h-40 lg:mb-20 lg:mt-8">
                   </img>
                 </Link>
               </div>
               
-              <div className="absolute top-0 pt-6 text-4xl text-white right-10 lg:mt-4 lg:mr-8 lg:right-20 sm:right-3 md:pt-10 lg:pt-12">
+              <div className="absolute top-0 z-10 text-3xl text-white lg:text-5xl sm:text-4xl pt-9 right-7 lg:mt-4 lg:mr-8 lg:right-20 sm:right-10 md:pt-10 lg:pt-12">
                 <button className=''>
                     <ion-icon name="menu" id="menu" onClick={onToggleMenu}></ion-icon>
                   </button>
               </div>
 
-              <div className={`flex flex-row-reverse items-center gap-7 text-[10px] pt-7 pr-20 sm:gap-10 lg:gap-20 sm:text-sm md:text-lg lg:text-xl nav-links lg:pt-14 lg:pr-52 sm:pr-28 md:pt-10 md:gap-16 sm:mt-5 transition duration-500 ${menuOpen ? 'visible opacity-100' : 'invisible opacity-0'}`}>
+              <div className={`flex flex-row-reverse z-0 items-center gap-7 text-[10px] pt-10 pr-20 sm:gap-10 lg:gap-20 sm:text-sm md:text-lg lg:text-2xl nav-links lg:pt-14 lg:pr-52 sm:pr-28 md:pt-10 md:gap-16 sm:mt-0 transition duration-500 ${menuOpen ? 'visible opacity-100' : 'invisible opacity-0'}`}>
                  
                  
                   <Link to="/contact-us">
@@ -53,7 +53,7 @@ const NavBar = () => {
                     <button className="text-white font-Italiana">Services</button>
                   </Link>
                   <Link to="/about-us">
-                    <button className="text-white font-Italiana">
+                    <button className="z-10 text-white font-Italiana">
                       About Us
                     </button>
                   </Link>
